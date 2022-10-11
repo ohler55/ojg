ed4c4a5f5573c7c3348bda5103f9d72466188d5e
Peter Ohler <peter@ohler.com>
2022-10-12 08:30:17 +0900

    Add fuzz test from nano

--- alt/builder_test.go
+++ alt/builder_test.go
@@ -3,6 +3,8 @@
 package alt_test
 
 import (
+	"fmt"
+	"runtime/debug"
 	"testing"
 
 	"github.com/ohler55/ojg/alt"
@@ -114,4 +116,88 @@ func TestBuilderErrors(t *testing.T) {
 
 	err = b.Value(true)
 	tt.Equal(t, "must have a key when pushing to an object", err.Error())
+}
+
+// This test can be runned with this command:
+//
+//     $ go test -fuzz=FuzzBuilder -fuzztime=1s
+//
+// Contributed by nono (Bruno Michel)
+//
+func FuzzBuilder(f *testing.F) {
+	defer func() {
+		if recover() != nil {
+			debug.PrintStack()
+		}
+	}()
+	f.Fuzz(func(t *testing.T, input []byte) {
+		defer func() {
+			if recover() != nil {
+				debug.PrintStack()
+			}
+		}()
+		var b alt.Builder
+		value := valueFromFuzzingInput(input)
+		buildFromValue(t, &b, value)
+		tt.Equal(t, value, b.Result())
+	})
+}
+
+func valueFromFuzzingInput(input []byte) interface{} {
+	switch {
+	case len(input) == 0 || input[0] == 0:
+		return nil
+	case input[0] == 1:
+		return true
+	case input[0] == 2:
+		return false
+	case ('a' <= input[0] && input[0] <= 'z') || ('A' <= input[0] && input[0] <= 'Z'):
+		return string(input[0])
+	case input[0] <= 160:
+		l := int(input[0] / 16)
+		array := make([]interface{}, 0, l)
+		for i := 0; i < l; i++ {
+			if len(input) > 0 {
+				input = input[1:]
+			}
+			item := valueFromFuzzingInput(input)
+			array = append(array, item)
+		}
+		return array
+	default:
+		l := int(input[0]/16 - 10)
+		obj := make(map[string]interface{})
+		for i := 0; i < l; i++ {
+			if len(input) > 0 {
+				input = input[1:]
+			}
+			item := valueFromFuzzingInput(input)
+			obj[fmt.Sprintf("%d", i)] = item
+		}
+		return obj
+	}
+}
+
+func buildFromValue(t *testing.T, b *alt.Builder, value interface{}, key ...string) {
+	switch value := value.(type) {
+	case map[string]interface{}:
+		err := b.Object(key...)
+		tt.Nil(t, err, "b.Object()")
+		for k, v := range value {
+			buildFromValue(t, b, v, k)
+		}
+		b.Pop()
+	case []interface{}:
+		err := b.Array(key...)
+		for _, v := range value {
+			buildFromValue(t, b, v)
+		}
+		b.Pop()
+		tt.Nil(t, err, "b.Array()")
+	case string, nil, bool:
+		err := b.Value(value, key...)
+		tt.Nil(t, err, "b.Value()")
+	default:
+		t.Fatal(fmt.Sprintf("invalid type: %T", value))
+	}
 }
