120d21e74bd0ea3bf0db4684a8ca8673e82ecc33
Peter Ohler <peter@ohler.com>
2023-02-15 19:50:35 -0500

    Add unit tests for alt.Filter

--- alt/filter.go
+++ alt/filter.go
@@ -3,6 +3,7 @@
 package alt
 
 import (
+	"reflect"
 	"strings"
 	"time"
 )
@@ -10,7 +11,7 @@ import (
 // Filter is a simple filter for matching against arbitrary date.
 type Filter map[string]any
 
-// NewFilter creates a new filter from the spec which whould be a map where
+// NewFilter creates a new filter from the spec which should be a map where
 // the keys are simple paths of keys delimited by the dot ('.') character. An
 // example is "top.child.grandchild". The matching will either match the key
 // when the data is traversed directly or in the case of a slice the elements
@@ -19,6 +20,11 @@ type Filter map[string]any
 // nested set of maps.
 func NewFilter(spec map[string]any) Filter {
 	f := Filter{}
+	f.add(spec)
+	return f
+}
+
+func (f Filter) add(spec map[string]any) {
 	for k, v := range spec {
 		path := strings.Split(k, ".")
 		f2 := f
@@ -30,84 +36,85 @@ func NewFilter(spec map[string]any) Filter {
 			}
 			f2 = sub
 		}
-		// TBD if value is map create sub filter
+		k2 := path[len(path)-1]
 		switch tv := v.(type) {
 		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-			f2[path[len(path)-1]], _ = asInt(tv)
+			f2[k2], _ = asInt(tv)
 		case float32, float64:
-			f2[path[len(path)-1]], _ = asFloat(tv)
+			f2[k2], _ = asFloat(tv)
+		case map[string]any:
+			sub, _ := f2[k2].(Filter)
+			if sub == nil {
+				sub = NewFilter(map[string]any{})
+				f2[k2] = sub
+			}
+			sub.add(tv)
 		default:
-			f2[path[len(path)-1]] = v
+			f2[k2] = v
 		}
 	}
-	return f
 }
 
 // Match returns true if the target matches the Filter.
-func (f Filter) Match(target any) bool {
-	tm, ok := target.(map[string]any)
-	if !ok {
-		return false
-	}
+func (f Filter) Match(data any) bool {
+	return match(f, data)
+}
+
+func match(target, data any) (same bool) {
 top:
-	for k, fv := range f {
-		switch tv := tm[k].(type) {
-		case map[string]any:
-			if sub, ok := fv.(Filter); ok {
-				if !sub.Match(tv) {
+	switch tv := data.(type) {
+	case map[string]any:
+		if f, ok := target.(Filter); ok {
+			same = true
+			for k, fv := range f {
+				if !match(fv, tv[k]) {
 					return false
 				}
 			}
-		case []any:
-			for _, v := range tv {
-				if f.Match(v) {
-					continue top
-				}
-			}
-		case nil:
-			if fv != nil {
-				return false
-			}
-		case bool:
-			if b, ok := fv.(bool); !ok || tv != b {
-				return false
-			}
-		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-			v, _ := asInt(tv)
-			if i, ok := asInt(fv); !ok || v != i {
-				return false
-			}
-		case float32, float64:
-			v, _ := asFloat(tv)
-			if ff, ok := asFloat(fv); !ok || v != ff {
-				return false
-			}
-		case string:
-			if fs, ok := fv.(string); !ok || fs != tv {
-				return false
-			}
-		case time.Time:
-			if ft, ok := fv.(time.Time); !ok || !ft.Equal(tv) {
-				return false
-			}
-		default:
-			// TBD reflect as map or slice
 		}
+	case []any:
+		for _, v := range tv {
+			if same = match(target, v); same {
+				break
+			}
+		}
+	case nil:
+		same = target == nil
+	case bool:
+		b, ok := target.(bool)
+		same = ok && tv == b
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		v, _ := asInt(tv)
+		i, ok := asInt(target)
+		same = ok && v == i
+	case float32, float64:
+		v, _ := asFloat(tv)
+		ff, ok := asFloat(target)
+		same = ok && v == ff
+	case string:
+		fs, ok := target.(string)
+		same = ok && fs == tv
+	case time.Time:
+		ft, ok := target.(time.Time)
+		same = ok && ft.Equal(tv)
+	case Simplifier:
+		data = tv.Simplify()
+		goto top
+	default:
+		data = reflectValue(reflect.ValueOf(tv), tv, &Options{})
+		goto top
 	}
-	return true
+	return
 }
 
 // Simplify returns a simplified representation of the Filter.
 func (f Filter) Simplify() any {
 	simple := map[string]any{}
 	for k, v := range f {
-		switch tv := v.(type) {
-		case map[string]any:
-			simple[k] = Filter(tv).Simplify()
-		case Filter:
-			simple[k] = tv.Simplify()
-		default:
-			simple[k] = tv
+		if f2, ok := v.(Filter); ok {
+			simple[k] = f2.Simplify()
+		} else {
+			simple[k] = v
 		}
 	}
 	return simple
--- alt/filter_test.go
+++ alt/filter_test.go
@@ -4,6 +4,7 @@ package alt_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/ohler55/ojg"
 	"github.com/ohler55/ojg/alt"
@@ -18,7 +19,7 @@ var prettyWriter = pretty.Writer{
 	SEN:      true,
 }
 
-func TestNewFilter(t *testing.T) {
+func TestNewFilterFlat(t *testing.T) {
 	f := alt.NewFilter(map[string]any{
 		"top.one":          1,
 		"top.two.son":      2,
@@ -28,7 +29,23 @@ func TestNewFilter(t *testing.T) {
 	tt.Equal(t, `{top: {one: 1 three: {child: 3} two: {daughter: 22 son: 2}}}`, string(prettyWriter.Encode(f)))
 }
 
-func TestFilterMatch(t *testing.T) {
+func TestNewFilterNested(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"top.one": 1,
+		"top": map[string]any{
+			"two": map[string]any{
+				"son":      2,
+				"daughter": 22,
+			},
+			"three": map[string]any{
+				"child": "x",
+			},
+		},
+	})
+	tt.Equal(t, `{top: {one: 1 three: {child: x} two: {daughter: 22 son: 2}}}`, string(prettyWriter.Encode(f)))
+}
+
+func TestFilterMatchMap(t *testing.T) {
 	f := alt.NewFilter(map[string]any{
 		"a.b": 2,
 		"a.c": 3,
@@ -48,4 +65,139 @@ func TestFilterMatch(t *testing.T) {
 			"c": 3,
 		},
 	}))
+
+	tt.Equal(t, false, f.Match(7))
+}
+
+func TestFilterMatchList1(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"a.b": 2,
+		"a.c": 3,
+	})
+	tt.Equal(t, true, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": 1,
+			"b": []any{1, 2, 3},
+			"c": 3,
+		},
+	}))
+	tt.Equal(t, false, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": 1,
+			"b": []any{1, 3, 5},
+			"c": 3,
+		},
+	}))
+}
+
+func TestFilterMatchList2(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"a.b": 2,
+		"a.c": 3,
+	})
+	tt.Equal(t, true, f.Match(map[string]any{
+		"a": []any{
+			map[string]any{
+				"x": 1,
+			},
+			map[string]any{
+				"a": 1,
+				"b": []any{1, 2, 3},
+				"c": 3,
+			},
+			map[string]any{
+				"y": 1,
+			},
+		},
+	}))
+}
+
+func TestFilterMatchFloat(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"a.b": 2.5,
+		"a.c": 3.0,
+	})
+	tt.Equal(t, true, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": 1.0,
+			"b": 2.5,
+			"c": 3,
+		},
+	}))
+	tt.Equal(t, false, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": 1.0,
+			"b": 2.4,
+			"c": 3,
+		},
+	}))
+}
+
+func TestFilterMatchBool(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"a.b": true,
+		"a.c": false,
+	})
+	tt.Equal(t, true, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": false,
+			"b": true,
+			"c": false,
+		},
+	}))
+	tt.Equal(t, false, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": false,
+			"b": false,
+			"c": false,
+		},
+	}))
+}
+
+func TestFilterMatchString(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"a.b": "xyz",
+		"a.c": "abc",
+	})
+	tt.Equal(t, true, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": "zzz",
+			"b": "xyz",
+			"c": "abc",
+		},
+	}))
+	tt.Equal(t, false, f.Match(map[string]any{
+		"a": map[string]any{
+			"a": "zzz",
+			"b": "abc",
+			"c": "abc",
+		},
+	}))
+}
+
+func TestFilterMatchSimplifier(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"type": "silly",
+		"val":  3,
+	})
+	tt.Equal(t, true, f.Match(&silly{val: 3}))
+	tt.Equal(t, false, f.Match(&silly{val: 2}))
+}
+
+func TestFilterMatchReflect(t *testing.T) {
+	f := alt.NewFilter(map[string]any{
+		"val":  3,
+		"nest": 2,
+	})
+	tt.Equal(t, true, f.Match(&Dummy{Val: 3, Nest: []any{1, 2, 4}}))
+	tt.Equal(t, false, f.Match(&Dummy{Val: 3, Nest: []any{1, 3, 5}}))
+}
+
+func TestFilterMatchTime(t *testing.T) {
+	now := time.Now()
+	f := alt.NewFilter(map[string]any{
+		"when": now,
+	})
+	tt.Equal(t, true, f.Match(map[string]any{"when": now}))
+	tt.Equal(t, false, f.Match(map[string]any{"when": time.Now()}))
 }
