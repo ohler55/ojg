4f986733b2544a4669101f8e271cb18151277b0b
Peter Ohler <peter@ohler.com>
2023-11-09 18:56:52 -0500

    Add option to jp.Walk for leaves only (#150)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 The structure and content of this file follows [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [1.20.3] - 2023-11-09
+### Added
+- Added an option to jp.Walk to just callback on leaves making the function more useable.
+
 ## [1.20.2] - 2023-10-30
 ### Fixed
 - A script of `@.x` is now read correctly as `@.x exists true`.
--- jp/walk_test.go
+++ jp/walk_test.go
@@ -8,16 +8,31 @@ import (
 
 	"github.com/ohler55/ojg/gen"
 	"github.com/ohler55/ojg/jp"
+	"github.com/ohler55/ojg/pretty"
 	"github.com/ohler55/ojg/sen"
 	"github.com/ohler55/ojg/tt"
 )
 
+type simple int
+
+func (s simple) Simplify() any {
+	return map[string]any{"x": int64(s)}
+}
+
+type other int
+
 func TestWalk(t *testing.T) {
-	data := map[string]any{"a": []any{1, 2, 3}, "b": nil}
+	data := map[string]any{"a": []any{1, 2, 3}, "b": nil, "c": simple(4), "d": other(5)}
 	var paths []string
 	jp.Walk(data, func(path jp.Expr, value any) { paths = append(paths, path.String()) })
 	sort.Strings(paths)
-	tt.Equal(t, `[$ $.a "$.a[0]" "$.a[1]" "$.a[2]" $.b]`, string(sen.Bytes(paths)))
+	tt.Equal(t, `[$ $.a "$.a[0]" "$.a[1]" "$.a[2]" $.b $.c $.c.x $.d]`, string(sen.Bytes(paths)))
+
+	leaves := map[string]any{}
+	jp.Walk(data, func(path jp.Expr, value any) {
+		leaves[path[1:].String()] = value
+	}, true)
+	tt.Equal(t, `{"a[0]": 1 "a[1]": 2 "a[2]": 3 b: null c.x: 4 d: 5}`, pretty.SEN(leaves))
 }
 
 func TestWalkNode(t *testing.T) {
