170613fd13da64664014362d964f3b2b43c8bffb
Peter Ohler <peter@ohler.com>
2024-10-21 21:40:38 -0400

    WIP

--- jp/frag.go
+++ jp/frag.go
@@ -11,4 +11,11 @@ type Frag interface {
 	Append(buf []byte, bracket, first bool) []byte
 
 	locate(pp Expr, data any, rest Expr, max int) (locs []Expr)
+
+	// Walk the matching elements in the data and call cb on the matches or
+	// follow on to the matching if not the last fragment in an
+	// expression. The rest argument is the rest of the expression after this
+	// fragment. The path is the normalized path up to this point. Data is the
+	// data element to act on.
+	Walk(rest, path Expr, data any, cb func(path Expr, data, parent any))
 }
--- jp/walk.go
+++ jp/walk.go
@@ -71,3 +71,71 @@ top:
 		cb(path, data)
 	}
 }
+
+// Walk the matching elements in the data and call cb on the matches or follow
+// on to the matching if not the last fragment in an expression. The rest
+// argument is the rest of the expression after this fragment. The path is the
+// normalized path up to this point. Data is the data element to act on.
+func (x Expr) Walk(data any, cb func(path Expr, data, parent any)) {
+	if 0 < len(x) {
+		x[0].Walk(x[1:], Expr{}, data, cb)
+	}
+}
+
+// Walk continues with the next in rest.
+func (f At) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	if 0 < len(rest) {
+		rest[0].Walk(rest[1:], path, data, cb)
+	} else {
+		cb(path, data, data)
+	}
+}
+
+func (f Bracket) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
+
+func (f Child) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
+
+func (f Descent) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
+
+func (f Filter) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
+
+func (f Nth) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+
+	// TBD if last ...
+	//     else cb
+
+	// TBD to delete in data, mod data at nth and update in parent (slice or map)
+	// how to deal with changes in index for unions and ranges?
+	//  keep data and just update parent each time.
+
+}
+
+// Walk continues with the next in rest.
+func (f Root) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	if 0 < len(rest) {
+		rest[0].Walk(rest[1:], path, data, cb)
+	} else {
+		cb(path, data, data)
+	}
+}
+
+func (f Slice) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
+
+func (f Union) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
+
+func (f Wildcard) Walk(rest, path Expr, data any, cb func(path Expr, data, parent any)) {
+	// TBD
+}
